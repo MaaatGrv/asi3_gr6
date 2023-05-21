0c9018317c0be5c49ecad97a6c2c3e262a3b45ad
Mathis Gorvien <mathis.gorvien@cpe.fr>
2023-05-21 14:07:44 +0200

    Init Gateway

---
+++ Gateway/src/test/java/com/asi1/gateway/Gateway/GatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.asi1.gateway.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Static/target/classes/static/js/home.js
@@ -0,0 +1,48 @@
+// Get the user data from the server
+function getUserData() {
+    fetch('/auth/user', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Not authorized');
+            }
+        })
+        .then(data => {
+            document.getElementById('user-account').textContent = data.account;
+            document.getElementById('user-login').textContent = data.login;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            window.location.replace('/login.html');
+        });
+}
+
+// Logout function
+function logout() {
+    fetch('/auth/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (response.ok) {
+                window.location.replace('/login.html');
+            } else {
+                console.error('Logout failed');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// Add event listener for the logout button
+document.getElementById('logout-btn').addEventListener('click', logout);
+
+// Get user data on page load
+getUserData();
