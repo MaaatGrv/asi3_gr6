efb66a921a81c2f01b5cc4dd2d383963af36c968
Mathis Gorvien <mathis.gorvien@cpe.fr>
2023-05-25 22:15:19 +0200

    init game ms

---
+++ GameMS/src/main/java/com/asi1/GameCard/game/GameApplication.java
@@ -0,0 +1,13 @@
+package com.asi1.game.GameApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
